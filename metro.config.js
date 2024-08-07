5e2eee3c5c62d93eb9f23c764453f5ec75032598
manish nagda <manishapps2020i@gmail.com>
2024-08-07 11:05:36 +0530

    Initial Commit

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+const {getDefaultConfig} = require('@react-native/metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+
+  return {
+    ...defaultConfig,
+    transformer: {
+      ...defaultConfig.transformer,
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      ...defaultConfig.resolver,
+      assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+    },
+  };
+})();
